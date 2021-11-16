363951c9a02dfef1bb6dfa41547a3658a321087d
YoannDelion <yoanndelion@gmail.com>
2021-11-16 17:49:25 +0100

    feat(useFetch) hook to fetch data

---
+++ hooks/useFetch.ts
@@ -0,0 +1,33 @@
+import { useEffect, useState } from 'react'
+
+export default function useFetch(url: string) {
+  const [data, setData] = useState()
+  const [loading, setLoading] = useState(true)
+  const [error, setError] = useState('')
+
+  useEffect(() => {
+    if (!url) return
+
+    setLoading(true)
+
+    async function fetchData() {
+      try {
+        const response = await fetch(url)
+        if (!response.ok) {
+          const { errorMessage } = await response.json()
+          throw new Error(errorMessage)
+        } else {
+          const data = await response.json()
+          setData(data)
+        }
+      } catch (error) {
+        setError(error.message)
+      } finally {
+        setLoading(false)
+      }
+    }
+    fetchData()
+  }, [url])
+
+  return { data, loading, error }
+}
--- screens/ClubListScreen.tsx
+++ screens/ClubListScreen.tsx
@@ -1,11 +1,24 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import { StyleSheet, View, Text } from 'react-native'
+import useFetch from '../hooks/useFetch'
 
 export default function ClubListScreen() {
+  const [clubs, setClubs] = useState([])
+
+  const { data, loading, error } = useFetch('https://api.mpg.football/api/data/championship-clubs')
+
+  useEffect(() => {
+    if (data) {
+      setClubs(Object.values(Object.values(data.championshipClubs)))
+    }
+  }, [data])
+
   return (
     <View style={styles.container}>
       <Text style={styles.title}>ClubListScreen</Text>
-      <View style={styles.separator} />
+      {clubs.map((x) => (
+        <Text key={x.id}>{x.id}</Text>
+      ))}
     </View>
   )
 }
